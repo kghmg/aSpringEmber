617d237c079406e4bfcebc110478e2d537561653
Hisham MG <hisham@khabtigrandhotel.com>
2023-08-19 02:32:58 +0300

    Dirty dirty

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,99 @@
+package org.example;
+
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            HashMap<String, String> parameters = new HashMap<>();
+            URL url = new URL("https://www.wikihow.com/Save-Money");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setDoOutput(true);
+
+            DataOutputStream dis = new DataOutputStream(con.getOutputStream());
+            dis.writeBytes(ParameterStringBuilder.getParameterString(parameters));
+            dis.flush();
+            dis.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
+@Controller
+@ResponseBody
+class NewJDKController{
+    private final ObservationRegistry observationRegistry;
+
+    public NewJDKController(ObservationRegistry observationRegistry) {
+        this.observationRegistry = observationRegistry;
+    }
+
+    @GetMapping("/factorial/{num}")
+    public int factorial(@PathVariable("num") int num){
+        return Observation.createNotStarted("factorial",observationRegistry)
+                .observe(()->calcFactorial(num));
+        //return calcFactorial(num);
+    }
+
+    private int calcFactorial(int num) {
+        if(num<1)
+            throw new BadNumberException("Number is negative");
+        //int res = 0;
+        if(num == 1){
+            return 1;
+        }else{
+            return num*(calcFactorial(num-1));
+        }
+    }
+}
+class BadNumberException extends RuntimeException{
+    public BadNumberException(String msg){
+        super(msg);
+    }
+}
+
+@ControllerAdvice
+class controllerAdvice{
+    @ExceptionHandler
+    ProblemDetail handle(BadNumberException exception, HttpServletRequest request){
+        request.getHeaderNames().asIterator()
+                .forEachRemaining(System.out::println);
+        ProblemDetail pd = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST.value());
+        pd.setDetail(exception.getLocalizedMessage());
+        return pd;
+    }
+}
+class ParameterStringBuilder{
+    public static String getParameterString(HashMap<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder res = new StringBuilder();
+        for(Map.Entry<String, String> entry:params.entrySet()){
+            res.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            res.append("=");
+            res.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+            res.append("&");
+        }
+        String resString = res.toString();
+        return resString.length() > 0
+                ? resString.substring(0, resString.length()-1)
+                : resString;
+
+    }
+}
